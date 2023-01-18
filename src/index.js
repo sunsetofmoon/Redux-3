a4fd29be9f80f3d695d50c60d6e4c06fd35c408c
Khalid <114188229+sunsetofmoon@users.noreply.github.com>
2023-01-18 17:37:13 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+
+const initialState = [
+  {
+    text: 'Учить JS',
+    done: true
+  },
+  {
+    text: 'Выучить React',
+    done: true
+  },
+  {
+    text: 'Стать богатым',
+    done: false
+  }
+
+]
+
+const reducer = (state = initialState, action) =>{
+  switch(action.type){
+      case 'delete' :
+          return state.filter((item, index) => {
+              if(action.payload === index){
+                  return false
+              }else{
+                  return true
+              }
+          })
+
+      default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+      <App />
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
